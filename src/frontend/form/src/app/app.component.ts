5b8e418791a8148cbcc3a7e4dc4dbfbfbc0b780a
trolldominic <dominic.troll@outlook.com>
2023-06-30 17:49:10 +0200

    Made changes to the frontend: It can now read the data fomr the API and use the Values to work with them.
    Added a download button to the frontend for the csv export.

--- src/frontend/form/src/app/app.component.ts
+++ src/frontend/form/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { saveAs } from 'file-saver';
 
 
 
@@ -12,6 +13,15 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent implements OnInit {
   title = 'ReactiveForms';
   reactiveForm: FormGroup;
+  apiResponse: any;
+  startDate: string;
+  endDate: string;
+  stockName: string;
+  startValue: number;
+  endValue: number;
+  investValue: number;
+  finalValue: number;
+  gainLossValue: number;
 
 
 
@@ -40,13 +50,64 @@ export class AppComponent implements OnInit {
 
 
 
-    this.http
+    /**this.http
       .post('http://localhost:8080/stocks', formData)
       .subscribe({
         next: (response) => console.log(response),
         error: (error) => console.log(error),
       });
+     **/
+    this.http
+      .post('http://localhost:8080/stocks', formData)
+      .subscribe(
+        (response: any) => {
+          this.apiResponse = response; // Store the response object
+          this.extractValues(response); // Call a method to extract and work with the values
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
   }
-}
 
+  private extractValues(response: any) {
+    this.startDate = response.startDate;
+    this.endDate = response.endDate;
+    this.stockName = response.stockName;
+    this.startValue = response.startValue;
+    this.endValue = response.endValue;
+    this.investValue = response.investValue;
+    this.finalValue = response.finalValue;
+    this.gainLossValue = response.gainLossValue;
+
+
+    // Work with the extracted values as needed
+    console.log(this.startDate);
+    console.log(this.endDate);
+    console.log(this.stockName);
+    console.log(this.startValue);
+    console.log(this.endValue);
+    console.log(this.investValue);
+    console.log(this.finalValue);
+    console.log(this.gainLossValue);
+
+    // Perform additional operations or calculations based on the extracted values
+    const profit = this.finalValue - this.investValue;
+    const roi = (profit / this.investValue) * 100;
+
+    console.log(profit);
+    console.log(roi);
+
+  }
+
+  downloadFile() {
+    const fileUrl = '1900-01-01_2999-01-01_Default_0.0.csv';
+
+    fetch(fileUrl)
+      .then(response => response.blob())
+      .then(blob => saveAs(blob, 'Your-Stock-Request.csv'))
+      .catch(error => console.error('Error downloading file:', error));
+  }
+
+}
 
--- src/main/java/ch/zhaw/oop/stocks/exporter/CsvExporterController.java
+++ src/main/java/ch/zhaw/oop/stocks/exporter/CsvExporterController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
+
 @RestController
 @RequestMapping("/exporter")
 public class CsvExporterController {
