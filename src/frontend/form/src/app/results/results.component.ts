e769839cce98d982f7ad3487305c6944f4222965
trolldominic <dominic.troll@outlook.com>
2023-07-01 17:13:36 +0200

    Made changes to the frontend: Added comments to the methods

--- src/frontend/form/src/app/results/results.component.ts
+++ src/frontend/form/src/app/results/results.component.ts
@@ -1,11 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { Chart, ChartData, ChartOptions } from 'chart.js';
 
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
   styleUrls: ['./results.component.css']
 })
-export class ResultsComponent {
+export class ResultsComponent implements AfterViewInit {
   @Input() startDate: string;
   @Input() endDate: string;
   @Input() stockName: string;
@@ -14,4 +15,41 @@ export class ResultsComponent {
   @Input() investValue: number;
   @Input() finalValue: number;
   @Input() gainLossValue: number;
+
+  @ViewChild('chartCanvas') chartCanvas: ElementRef;
+  private chart: Chart;
+
+  ngAfterViewInit() {
+    this.createChart();
+  }
+
+  private createChart() {
+    const canvas: HTMLCanvasElement = this.chartCanvas.nativeElement;
+    const ctx = canvas.getContext('2d');
+
+    const data: ChartData = {
+      labels: ['Start', 'End'],
+      datasets: [{
+        label: 'Stock Value',
+        data: [this.startValue, this.endValue],
+        backgroundColor: 'rgba(0, 123, 255, 0.5)',
+        borderColor: 'rgba(0, 123, 255, 1)',
+        borderWidth: 1
+      }]
+    };
+
+    const options: ChartOptions = {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    };
+
+    this.chart = new Chart(ctx, {
+      type: 'line',
+      data,
+      options
+    });
+  }
 }
