1fd484153cc0e4414804ec8bd652554b58e1b55d
trolldominic <dominic.troll@outlook.com>
2023-07-02 14:03:59 +0200

    Added some comments to all the frontend files.

--- src/frontend/form/src/app/app-routing.module.ts
+++ src/frontend/form/src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import {AppComponent} from "./app.component";
 import {ResultsComponent} from "./results/results.component";
 
 
-
+// TRD: The routes array contains the routes of the application. The first route is the home route, which is the route of the AppComponent.
+// the second route is the results route, which is the route of the ResultsComponent.
 const routes: Routes = [
  // { path: '', component: AppComponent }, // Home (Form) route
  // { path: 'results', component: ResultsComponent }, // Results site route
 ];
 
-
+// TRD: The AppRoutingModule is the routing module of the application. It imports the RouterModule and defines the routes of the application.
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
