028b4abf8e5ee0d5f821ac7648797f4621252181
AdrianSchaerer <adrian.sch@bluewin.ch>
2023-07-04 18:06:09 +0200

    Revert test package api

---
+++ src/test/java/ch/zhaw/oop/stocks/api/pojo/SimpleTestCaseJsonPOJO.java
@@ -0,0 +1,14 @@
+package ch.zhaw.oop.stocks.api.pojo;
+
+public class SimpleTestCaseJsonPOJO {
+
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
