aa530771e77d88dc2a5f329762709b19d6fa2734
AdrianSchaerer <adrian.sch@bluewin.ch>
2023-07-04 18:11:19 +0200

    Reversing tests for package api to older version

---
+++ src/test/java/ch/zhaw/oop/stocks/api/ApiStockControllerTest.java
@@ -0,0 +1,14 @@
+package ch.zhaw.oop.stocks.api;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApiStockControllerTest {
+
+//    ApiStockController apiStockController = new ApiStockController();
+//    @Test
+//    void createStock() {
+//        String stockList = apiStockController.createStock();
+//    }
+}
